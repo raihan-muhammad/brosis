2223bbe4e51fbaa8cfed37f793782bb2a6d54e5a
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2020-11-11 20:03:23 +0700

    add: fix animation in navbar mobile

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,6 +7,7 @@ const iconBurger = document.getElementById('icon-burger');
 const csBg = document.getElementById('cs-bg');
 const menuNav = document.getElementById('mb-menu-nav');
 const textCloseNav = document.getElementById('text-close-nav');
+const listMenu = document.getElementById('mb-list-menu');
 
 let statusBtn = false;
 
@@ -15,11 +16,13 @@ btnBurger.addEventListener('click', () => {
   if (statusBtn === false) {
     statusBtn = true;
     menuNav.classList.add('mb-nav-active');
+    listMenu.style.display = 'block';
     btnBurger.style.transition = 'all 0.8s ease';
     menuNav.style.backgroundColor = 'rgb(32 32 32 / 67%)';
     bgHeader.style.display = 'none';
     iconBurger.removeAttribute('class');
     iconBurger.setAttribute('class', 'ri-close-fill');
+    textCloseNav.style.display = 'block';
     csBg.style.boxShadow = '0 1px 6px #bfbbbb';
   } else {
     statusBtn = false;
@@ -27,6 +30,8 @@ btnBurger.addEventListener('click', () => {
     btnBurger.style.transition = 'all 0.8s ease';
     menuNav.style.backgroundColor = '#fff';
     bgHeader.style.display = 'block';
+    listMenu.style.display = 'none';
+    textCloseNav.style.display = 'none';
     iconBurger.removeAttribute('class');
     iconBurger.setAttribute('class', 'ri-menu-fill');
     csBg.style.boxShadow = '0 1px 6px #bfbbbb';
@@ -40,6 +45,9 @@ textCloseNav.addEventListener('click', () => {
     btnBurger.style.transition = 'all 0.8s ease';
     body.style.backgroundColor = 'rgb(32 32 32 / 67%)';
     bgHeader.style.display = 'none';
+    listMenu.style.display = 'block';
+    textCloseNav.style.display = 'block';
+
     iconBurger.removeAttribute('class');
     iconBurger.setAttribute('class', 'ri-close-fill');
     csBg.style.boxShadow = '0 1px 6px #bfbbbb';
@@ -49,6 +57,8 @@ textCloseNav.addEventListener('click', () => {
     btnBurger.style.transition = 'all 0.8s ease';
     body.style.backgroundColor = '#fff';
     bgHeader.style.display = 'block';
+    listMenu.style.display = 'none';
+    textCloseNav.style.display = 'none';
     iconBurger.removeAttribute('class');
     iconBurger.setAttribute('class', 'ri-menu-fill');
     csBg.style.boxShadow = '0 1px 6px #bfbbbb';
